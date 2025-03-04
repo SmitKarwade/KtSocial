34107a382b4491e127abab4ec7445068d6c501f7
SmitKarwade <smitkarwade31@gmail.com>
2025-03-04 16:35:59 +0530

    Added paging library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,10 @@ dependencies {
 
     implementation("com.google.dagger:hilt-android:2.51.1")
     kapt("com.google.dagger:hilt-android-compiler:2.51.1")
+    //paging
+    val paging_version = "3.3.6"
+
+    implementation("androidx.paging:paging-runtime:$paging_version")
 
 
     // AndroidX Core
