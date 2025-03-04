6b3c173cd0113c2ed1992fd550e90deb799d37ac
SmitKarwade <smitkarwade31@gmail.com>
2025-03-04 16:36:19 +0530

    Changed kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,6 @@ plugins {
     alias(libs.plugins.android.application) apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
-    kotlin("android") version "2.0.0" apply false
-    kotlin("kapt") version "2.0.0" apply false
+    kotlin("android") version "2.1.10" apply false
+    kotlin("kapt") version "2.1.10" apply false
 }
